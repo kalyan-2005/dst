4c64bcaafcd8b1b96c3a9d3138007ad652df15a5
Kalyan <kalyantingani@gmail.com>
2024-09-12 00:32:19 +0530

    fix build

--- src/app/(dashboard)/profile/page.tsx
+++ src/app/(dashboard)/profile/page.tsx
@@ -4,10 +4,13 @@ import PersonalProfile from "@/components/personal-profile";
 
 async function page() {
   const currentUser = await getCurrentUser();
-  const { address, mobile, registeredName }: any = await getAddress();
+  const address: any = await getAddress();
+
+  if(!currentUser || !address || !address.address || !address.mobile || !address.registeredName) return null
+
   return (
     <div className="mx-32 flex mt-8 gap-4">
-      <PersonalProfile currentUser={currentUser} address={address} mobile={mobile} registeredName={registeredName} />
+      <PersonalProfile currentUser={currentUser} address={address.address} mobile={address.mobile} registeredName={address.registeredName} />
       <div className="shadow-md w-full p-4 rounded">Numbers and doughnut chart</div>
     </div>
   );
